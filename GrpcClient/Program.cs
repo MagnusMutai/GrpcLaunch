c86c6d55daffa83c667e425f2eaeb92b9bcc4a63
Gilly <mutaigilly02@gmail.com>
2023-12-02 16:05:16 +0300

    I know grpc better

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest { Name = "Magnus"};
+
+            //var channel = GrpcChannel.ForAddress("https://localhost:7098");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
